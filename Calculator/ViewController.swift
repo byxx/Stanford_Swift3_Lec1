e3081fa6c75b0d86fd4210e6c2f3af210639bdbf
byxx <byxx@users.noreply.github.com>
2017-07-12 01:38:39 +0200

    firstCommit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //instance variables are properties
+    
+    //BTW :Type form(external name) startx(internal name)
+   // func drawHorizontalLine(from startX: Double to endX: Double, using color: UIColor)
+    
+    //App Crash during development is great
+    //Swift Method
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    //Optionals are initialized by Automatically set to nil
+    @IBOutlet weak var display: UILabel!
+    
+    
+    //Set Label to 0 in Attribute Inspector
+    //Use Autoshrink in Attribute Inspector
+    @IBAction func touchDigit(_ sender: UIButton) {
+    let digit = sender.currentTitle!
+        
+        if userIsInTheMiddleOfTyping {
+    let textCurrentlyInDisplay = display.text!
+        display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+            }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Computed property (calculated)
+    var displayValue: Double {
+        get {
+         return Double(display.text!)!
+        }
+        set {
+        display.text = String(newValue)
+        }
     }
-
-
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        //Fix:
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                if display.text != nil {
+                displayValue = sqrt(displayValue)
+                }
+            default:
+                break
+                
+            }
+        }
+    }
+    
 }
-
